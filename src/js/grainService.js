9f952a68b9cc8548b54a2bba16f820a73032b549
Shane Bergin <shanealanbergin@gmail.com>
2020-01-01 15:18:18 +0000

    remove garbage collection

--- src/js/grainService.js
+++ src/js/grainService.js
@@ -1,7 +1,5 @@
 import delayService from './delayService';
 
-const SECONDS_TO_MS_WEIGHT = 1000;
-
 const createSource = function createSource(context, buffer, params) {
   const source = context.createBufferSource();
   source.playbackRate.value = params.playbackRate;
@@ -17,9 +15,6 @@ const getOffset = function getOffset(offset, spread) {
 /**
  * Create a grain by sending the provided
  * params to the audio context destination
- *
- * A garbage collection is ran to kill the gain and
- * source objects.
  * @param {AudioContext} context
  * @param {AudioBuffer} buffer
  * @param {Object} params
@@ -37,17 +32,6 @@ const playGrain = function playGrain(context, buffer, params = {}) {
   gain.gain.linearRampToValueAtTime(0, now + params.attack + params.release);
 
   source.start(now, getOffset(params.offset, params.spread), params.duration);
-
-  const garbageCollectTimeout = (
-    params.duration
-    + params.attack
-    + params.release)
-    * SECONDS_TO_MS_WEIGHT
-    + params.interval;
-  setTimeout(() => {
-    source.stop();
-    gain.disconnect();
-  }, garbageCollectTimeout);
 };
 
 const createGrainDelay = function createGrainDelay(context, buffer, params) {
